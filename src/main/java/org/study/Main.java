4181028d4356e9b4fa42e31e283a78eea067ddc9
SangJung <sangjung245@gmail.com>
2024-04-14 19:33:29 +0900

    [DOCS] gui xserver 세팅
    gui 테스트
    xserver 환경변수 추가

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -1,8 +1,19 @@
 package org.study;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
-        System.out.println("current path: "+System.getProperty("user.dir"));
+public class Main extends JFrame {
+
+    Main(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("test");
+        setLocation(100,100);
+        setSize(300,300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("current path: " + System.getProperty("user.dir"));
+        new Main();
     }
 }
