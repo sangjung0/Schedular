4d3e7b172a6eef2f06ee69cfd6dfe98690119fcd
sangjung <sangjung245@gmail.com>
2024-05-05 11:08:03 +0000

    [ADD] STable class
    table을 관리하는 추상 클래스. 테이블의 공통된 부분을 정의함. 특히 각 칸의 최소 너비를 지정

---
+++ src/main/java/org/study/view/table/STable.java
@@ -0,0 +1,31 @@
+package org.study.view.table;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * JTable을 확장한 테이블 클래스
+ */
+public abstract class STable extends JTable {
+
+    private static final int TABLE_COLUMN_MIN_WIDTH = 100;
+
+    /**
+     * 컬럼 제목을 받아 테이블 생성
+     * @param columns 컬럼 제목
+     */
+    protected STable(String[] columns){
+        super(new DefaultTableModel(columns, 0));
+        setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        setTableColumnMinWidth();
+    }
+
+    /**
+     * 각 컬럼의 최소 너비 지정
+     */
+    protected void setTableColumnMinWidth(){
+        for (int i = 0; i < columnModel.getColumnCount(); i++){
+            columnModel.getColumn(i).setMinWidth(TABLE_COLUMN_MIN_WIDTH);
+        }
+    }
+}
