1d2d093e5b3e092ec717a7d6617f8dd032b678d3
sangjung <sangjung245@gmail.com>
2024-05-05 10:54:26 +0000

    [TEST] Main 클래스 테스트 구문 제거
    gui 테스트 구문 제거
    SMain gui 클래스 실행

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -1,19 +1,10 @@
 package org.study;
 
-import javax.swing.*;
+import org.study.view.SMain;
 
-public class Main extends JFrame {
-
-    Main(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("test");
-        setLocation(100,100);
-        setSize(300,300);
-        setVisible(true);
-    }
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("current path: " + System.getProperty("user.dir"));
-        new Main();
+        new SMain();
     }
 }
