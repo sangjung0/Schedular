83bb2a3eccbbb1309a48cf2d5c31c6681a92b8b1
sangjung <sangjung245@gmail.com>
2024-05-05 11:05:05 +0000

    [ADD] SLabel class
    gui의 모든 Label 디자인을 관리하기 위해 생성.
    디자인 기능 넣기 전

---
+++ src/main/java/org/study/view/label/SLabel.java
@@ -0,0 +1,12 @@
+package org.study.view.label;
+
+import javax.swing.*;
+
+/**
+ * JLabel을 확장한 SLabel
+ */
+public abstract class SLabel extends JLabel {
+    public SLabel(){
+        super();
+    }
+}
