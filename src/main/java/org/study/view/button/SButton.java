e06d7587ad368655a63da8f5659abcc7edf47f37
sangjung <sangjung245@gmail.com>
2024-05-05 11:03:01 +0000

    [ADD] SButton class
    SButton 추가. GUI의 버튼들의 디자인을 동일하게 하기 위해서 생성. 아직 적용 전

---
+++ src/main/java/org/study/view/button/SButton.java
@@ -0,0 +1,12 @@
+package org.study.view.button;
+
+import javax.swing.*;
+
+/**
+ * JButton을 확장하는 추상클래스
+ */
+public abstract class SButton extends JButton {
+    public SButton(String name){
+        super(name);
+    }
+}
