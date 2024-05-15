2a529ae4817ace6ef9a3e2d51576699969a03c2a
sangjung <sangjung245@gmail.com>
2024-05-15 14:44:08 +0000

    [FEAT] Scheduler 추상 클래스 추가

--- src/main/java/org/study/scheduler/Scheduler.java
+++ src/main/java/org/study/scheduler/Scheduler.java
@@ -1,10 +1,17 @@
 package org.study.scheduler;
 
-import org.study.GanttData;
 import org.study.SProcess;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
 
-public interface Scheduler {
-    public void run(ArrayList<SProcess> processes, ArrayList<GanttData> ganttData);
+public abstract class Scheduler {
+
+    protected abstract int getRunTime(SProcess currentProcess);
+    protected abstract void addReadyQ(SProcess process);
+    protected abstract boolean readyQIsEmpty();
+    protected abstract SProcess pollReadyQ();
+    protected abstract SProcess peekReadyQ();
+    protected abstract Iterator<SProcess> iterableReadyQ();
+
 }
