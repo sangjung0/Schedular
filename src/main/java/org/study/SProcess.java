9a65e474243b95d86e0a251e8a1a5bac4fba858e
sangjung <sangjung245@gmail.com>
2024-05-15 14:37:45 +0000

    [FEAT] SProcess Attribute 추가
    모든 속성을 대상으로 하는 생성자 추가
    copy 함수 추가
    turnAroundTime 과 responseTime 속성 추가

--- src/main/java/org/study/SProcess.java
+++ src/main/java/org/study/SProcess.java
@@ -6,16 +6,54 @@ public class SProcess {
     private final int burstTime;
     private final int priority;
     private final int arrivalTime;
-    private int executionTime = 0;
-    private int waitingTime = 0;
+    private int responseTime;
+    private int executionTime;
+    private int waitingTime;
+    private int turnAroundTime;
 
     public SProcess(String name, int arrivalTime, int burstTime, int priority){
         this.name = name;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.priority = priority;
+        executionTime = 0;
+        waitingTime = 0;
+        responseTime = 0;
+        turnAroundTime = 0;
     }
 
+    public SProcess(String name, int arrivalTime, int burstTime, int priority, int executionTime, int waitingTime, int responseTime, int turnAroundTime){
+        this.name = name;
+        this.burstTime = burstTime;
+        this.priority = priority;
+        this.arrivalTime = arrivalTime;
+        this.executionTime = executionTime;
+        this.waitingTime = waitingTime;
+        this.responseTime = responseTime;
+        this.turnAroundTime = turnAroundTime;
+    }
+
+    public SProcess copy(){
+        return new SProcess(
+                name,
+                burstTime,
+                priority,
+                arrivalTime,
+                executionTime,
+                waitingTime,
+                responseTime,
+                turnAroundTime
+        );
+    }
+
+    public void setTurnAroundTime(int turnAroundTime) {
+        this.turnAroundTime = turnAroundTime;
+    }
+    public int getTurnAroundTime() {
+        return turnAroundTime;
+    }
+    public void setResponseTime(int time){responseTime = time;}
+    public int getResponseTime(){return responseTime;}
     public int getArrivalTime(){
         return arrivalTime;
     }
