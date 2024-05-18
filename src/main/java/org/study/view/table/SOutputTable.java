406cd9ab21145fa4d0f10cbca6247e17212c7242
sangjung <sangjung245@gmail.com>
2024-05-18 08:14:38 +0000

    [TEST] SOutputTable 테스트 코드 제거

--- src/main/java/org/study/view/table/SOutputTable.java
+++ src/main/java/org/study/view/table/SOutputTable.java
@@ -10,46 +10,32 @@ import java.util.ArrayList;
  */
 public class SOutputTable extends STable{
 
-    private static final String[] COLUMNS_SUMMARY = {"Process", "ExecutionTime", "WaitingTime"};
+    private static final String[] COLUMNS_SUMMARY = {"Process", "Execution Time", "Waiting Time", "Turnaround Time", "Response Time"};
 
-    private int totalExecutionTime = 0;
-    private int averageWaitingTime = 0;
+    private final ArrayList<SProcess> processes;
 
     /**
      * table 생성
      */
-    public SOutputTable(){
+    public SOutputTable(ArrayList<SProcess> processes){
         super(COLUMNS_SUMMARY);
-    }
-
-    public int getTotalExecutionTime() {
-        return totalExecutionTime;
-    }
-
-    public int getAverageWaitingTime() {
-        return averageWaitingTime;
+        this.processes = processes;
     }
 
     /**
-     * 테이블에 데이터 입력
-     * @param list SProcess 객체를 담고있는 리스트
+     * 테이블 갱신
      */
-    public void setData(ArrayList<SProcess> list){
-        Object[][] data = new Object[list.size()][COLUMNS_SUMMARY.length];
+    public void reRandTable(){
+        Object[][] data = new Object[processes.size()][COLUMNS_SUMMARY.length];
 
-        // 임시로 전체 실행시간과 평균 대기시간 계산하는 로직을 추가한 상태.
-        totalExecutionTime = 0;
-        averageWaitingTime = 0;
-        for(int i = 0; i < list.size(); i++){
-            SProcess process = list.get(i);
+        for(int i = 0; i < processes.size(); i++){
+            SProcess process = processes.get(i);
             data[i][0] = process.getName();
             data[i][1] = process.getExecutionTime();
             data[i][2] = process.getWaitingTime();
-            totalExecutionTime += process.getExecutionTime();
-            averageWaitingTime += process.getWaitingTime();
+            data[i][3] = process.getTurnaroundTime();
+            data[i][4] = process.getResponseTime();
         }
-        totalExecutionTime /= list.size();
-        averageWaitingTime /= list.size();
 
         ((DefaultTableModel) getModel()).setDataVector(data, COLUMNS_SUMMARY);
         setTableColumnMinWidth();
