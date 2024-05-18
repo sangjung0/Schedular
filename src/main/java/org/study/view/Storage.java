d90c9d32ea3455393d0bd6f9197a4c3c0dbe2af8
sangjung <sangjung245@gmail.com>
2024-05-18 08:13:26 +0000

    [FEAT] Storage 저장 범위 확장
    Storage 클래스에서 모든 데이터를 저장함. 또한 평균값 구하는 함수를 지원

--- src/main/java/org/study/view/Storage.java
+++ src/main/java/org/study/view/Storage.java
@@ -1,5 +1,6 @@
 package org.study.view;
 
+import org.study.GanttData;
 import org.study.SProcess;
 
 import java.io.*;
@@ -9,25 +10,50 @@ import java.util.ArrayList;
 /**
  * ArrayList<SProcess> 를 확장하는 리스트
  */
-public class Storage extends ArrayList<SProcess>{
+public class Storage{
+
+    private final ArrayList<SProcess> processes;
+    private final ArrayList<SProcess> scheduled;
+    private final ArrayList<GanttData> ganttData;
+
     public Storage(){
-        super();
+        processes = new ArrayList<SProcess>();
+        ganttData = new ArrayList<GanttData>();
+        scheduled = new ArrayList<SProcess>();
+    }
+
+    public ArrayList<SProcess> getProcesses() {return processes;}
+    public ArrayList<GanttData> getGanttData() {return ganttData;}
+    public ArrayList<SProcess> getScheduled() {return scheduled;}
+
+    public void clearProcesses(){processes.clear();}
+    public void clearGanttData(){ganttData.clear();}
+    public void clearScheduled(){scheduled.clear();}
+    public ArrayList<SProcess> newScheduled(){
+        clearScheduled();
+        for(SProcess p: processes) scheduled.add(p.copy());
+        return scheduled;
+    }
+    public ArrayList<GanttData> newGanttData(){
+        clearGanttData();
+        return ganttData;
     }
 
     /**
      * 파일을 읽어 리스트에 SProcess 객체 추가.
      * 파일의 각 행은 아래와 같아야 함.
-     * "{name} {number} {arrivalTime} {executionTime} {priority}\n"
+     * "{name} {arrivalTime} {executionTime} {priority}\n"
      * @param file 읽을 파일
      * @return 오류 없으면 false
      */
     public boolean read(File file) {
+        clearProcesses();
         try{
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
             String line;
             while((line = reader.readLine()) != null){
                 String[] data = line.split(" ");
-                add(new SProcess(data[0] + data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4])));
+                processes.add(new SProcess(data[0], Integer.parseInt(data[1]), Integer.parseInt(data[2]), Integer.parseInt(data[3])));
             }
             return false;
         }catch (FileNotFoundException e){
@@ -36,5 +62,34 @@ public class Storage extends ArrayList<SProcess>{
             e.printStackTrace();
         }
         return true;
+    }
+
+    public static int averageResponseTime(ArrayList<SProcess> data){
+        int sum = 0;
+        for(SProcess p: data){
+            sum += p.getResponseTime();
+        }
+        return sum / data.size();
+    }
+    public static int averageWaitingTime(ArrayList<SProcess> data){
+        int sum = 0;
+        for(SProcess p: data){
+            sum += p.getWaitingTime();
+        }
+        return sum / data.size();
+    }
+    public static int averageTurnaroundTime(ArrayList<SProcess> data){
+        int sum = 0;
+        for(SProcess p: data){
+            sum += p.getTurnaroundTime();
+        }
+        return sum / data.size();
+    }
+    public static int totalExecutionTime(ArrayList<SProcess> data){
+        int sum = 0;
+        for(SProcess p: data){
+            sum += p.getExecutionTime();
+        }
+        return sum;
     }
 }
