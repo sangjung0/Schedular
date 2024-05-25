13ff6795b0df808cc4104bfea76c7139786e553c
sangjung <sangjung245@gmail.com>
2024-05-25 11:52:03 +0000

    [MODIFY] 중복된 구조 일반화

--- src/main/java/org/study/view/label/SAverageWaitingTime.java
+++ src/main/java/org/study/view/label/SAverageWaitingTime.java
@@ -6,37 +6,10 @@ package org.study.view.label;
 public class SAverageWaitingTime extends SLabel{
     private static final String NAME = "average waiting time";
 
-    private int time = 0;
-
     /**
      * 버튼 초기화
      */
     public SAverageWaitingTime() {
-        super();
-        setTime();
-    }
-
-    /**
-     * "average waiting time: {time}" 으로 초기화
-     * time 은 기존 time 값.
-     */
-    public void setTime(){
-        setText(NAME+": "+time);
-    }
-
-    /**
-     * "average waiting time: {time}" 으로 초기화
-     * @param time 시간
-     */
-    public void setTime(int time){
-        this.time = time;
-        setTime();
-    }
-
-    /**
-     * "average waiting time: 0" 으로 초기화
-     */
-    public void setZero(){
-        setTime(0);
+        super(NAME);
     }
 }
--- src/main/java/org/study/view/label/SLabel.java
+++ src/main/java/org/study/view/label/SLabel.java
@@ -5,8 +5,39 @@ import javax.swing.*;
 /**
  * JLabel을 확장한 SLabel
  */
-public abstract class SLabel extends JLabel {
-    public SLabel(){
+public abstract class SLabel extends SLabelStyle {
+
+    private int count = 0;
+    private String name = "";
+
+    public SLabel(String name){
         super();
+        this.name = name;
+        setTime();
     }
+
+    /**
+     * "{name}: {count}" 으로 초기화
+     * count 은 기존 count 값.
+     */
+    public void setTime(){
+        setText(name+": "+count);
+    }
+
+    /**
+     * "{name: {count}" 으로 초기화
+     * @param count 정수
+     */
+    public void setTime(int count){
+        this.count = count;
+        setTime();
+    }
+
+    /**
+     * "{name: 0" 으로 초기화
+     */
+    public void setZero(){
+        setTime(0);
+    }
+
 }
