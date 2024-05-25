d923a254fcc911a6758c3f0aa64d93519cbd97b6
sangjung <sangjung245@gmail.com>
2024-05-25 11:54:36 +0000

    [FEAT] 라벨 추가

---
+++ src/main/java/org/study/view/label/SInput.java
@@ -0,0 +1,8 @@
+package org.study.view.label;
+
+public class SInput extends SLabelStyle{
+    private static final String NAME = "input";
+    public SInput(){
+        super(NAME);
+    }
+}
---
+++ src/main/java/org/study/view/label/SOutput.java
@@ -0,0 +1,8 @@
+package org.study.view.label;
+
+public class SOutput extends SLabelStyle{
+    private static final String NAME = "output";
+    public SOutput(){
+        super(NAME);
+    }
+}
